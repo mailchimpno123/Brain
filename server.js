86d03fa9514feeeddac85ab2430131c25cd1369f
Dave Carbonara <carbonara@Daves-MacBook-Pro.local>
2022-12-16 23:33:04 +0100

    123

--- server.js
+++ server.js
@@ -8,11 +8,14 @@ const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
 
+
   const db = knex({ 
 	client: 'pg',
  	connection: {
     connectionString : process.env.DATABASE_URL,
-    ssl: true,
+    ssl: {
+    	rejectUnauthorized: false
+    }
 	}
 });
 
