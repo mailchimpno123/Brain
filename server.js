57dc18be9aa751ae0a6b52bf7f3c189df9f8d4bf
Dave Carbonara <carbonara@Daves-MacBook-Pro.local>
2022-12-16 23:00:12 +0100

    changing host

--- server.js
+++ server.js
@@ -11,10 +11,8 @@ const image = require('./controllers/image');
   const db = knex({ 
 	client: 'pg',
  	connection: {
-    host : 'postgresql-colorful-60749',
-    user : 'carbonara',
-    password : '',
-    database : 'smart-brain'
+    host : process.env.DATABASE_URL,
+    ssl: true,
 	}
 });
 
