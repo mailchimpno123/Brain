be9a40c342634f5befd318a21813cb3bce971185
Dave Carbonara <carbonara@Daves-MacBook-Pro.local>
2022-12-18 02:57:50 +0100

    fix

--- server.js
+++ server.js
@@ -24,11 +24,12 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get("/", (req, res) => {res.send("Working!")})
-app.post("/signin", signin.handleSignin(db, bcrypt))
-app.post("/register", (req, res) => { register.handleRegister(req, res, db, bcrypt)})
-app.get("/profile/:id", (req, res) => { profile.handleProfileGet(req, res, db)})
-app.post("/imageurl", (req, res) => { image.handleApiCall(req, res)})
+app.get('/', (req, res)=> { res.send(db.users) })
+app.post('/signin', signin.handleSignin(db, bcrypt))
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
+app.put('/image', (req, res) => { image.handleImage(req, res, db)})
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
 app.listen(process.env.PORT || 3000, () => {
 	console.log("App is running on port ${process.env.PORT}");
